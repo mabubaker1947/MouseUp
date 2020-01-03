6de20455e686b0ef9c5bf425b062c633d3c02985
Muhammad Abubaker <7279385+mabubaker1947@users.noreply.github.com>
2020-01-03 15:55:38 +0000

    Added initial source code
    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MouseUpConsoleApp
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        private const int STD_OUTPUT_HANDLE = -11;
+        const int LEFT_DOWN = 0x00000002;
+        const int LEFT_UP = 0x00000004;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //mouse_event(LEFT_DOWN, 0, 0, 0, 0);
+                mouse_event(LEFT_UP, 0, 0, 0, 0);
+                Thread.Sleep(299790); // wait for just before 5 minutes count is completed to lock the PC
+            }
+        }
+    }
+}
